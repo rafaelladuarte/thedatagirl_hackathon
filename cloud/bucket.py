0f24beedce7a100c0bb0f0eba0817abaade8a417
diascarolina <33383955+diascarolina@users.noreply.github.com>
2021-09-22 01:39:21 -0300

    update get_csv

--- cloud/bucket.py
+++ cloud/bucket.py
@@ -1,11 +1,12 @@
 from google.cloud import storage
 import dask.dataframe as dd
-import os
+
+bucket_name = 'thedatagirls-hackathon-a3'
 
 class MyStorage():
     def __init__(self):
         self.client = storage.Client()
-        self.bucket = self.client.get_bucket('thedatagirls-hackathon-a3')
+        self.bucket = self.client.get_bucket(bucket_name)
     
     def send_csv(self, remote_file, local_file):
         """
@@ -16,5 +17,16 @@ class MyStorage():
         blob.upload_from_filename(local_file)
 
     def get_csv(self, remote_file):
-        df = dd.read_csv(remote_file)
-        return df    
+        df = dd.read_csv(f'gs://{bucket_name}/{remote_file}', encoding = 'iso-8859-1', header = None)
+        return df
+
+    def list_files(self):
+        file_list = []
+        for blob in self.client.list_blobs(bucket_name):
+            file_list.append(blob.name)
+        return file_list
+
+if __name__ == "__main__":
+    cloud = MyStorage()
+    df = cloud.get_csv("UALS_2021-09-22_01-07-48.557902.csv")
+    print(df.head())
