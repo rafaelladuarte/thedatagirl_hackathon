ebadee43d7f376ffb879d26a275bdcac376cf27a
diascarolina <33383955+diascarolina@users.noreply.github.com>
2021-09-22 00:48:21 -0300

    minimal changes

--- cloud/bucket.py
+++ cloud/bucket.py
@@ -7,10 +7,14 @@ class MyStorage():
         self.client = storage.Client()
         self.bucket = self.client.get_bucket('thedatagirls-hackathon-a3')
     
-    def send_csv(self,remote_file,local_file):
+    def send_csv(self, remote_file, local_file):
+        """
+        Uploads a file to the bucket.
+        Source: https://cloud.google.com/storage/docs/uploading-objects
+        """
         blob = self.bucket.blob(remote_file)
         blob.upload_from_filename(local_file)
 
-    def get_csv(self,remote_file):
+    def get_csv(self, remote_file):
         df = dd.read_csv(remote_file)
-        return df   
+        return df    
