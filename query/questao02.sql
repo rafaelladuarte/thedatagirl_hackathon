3b9de7a2ee90de9700af239b64dc832737fdce04
elladarte <rafaella.d.d.carvalho@gmail.com>
2021-09-25 00:29:28 -0300

    Add query question 2

--- query/questao02.sql
+++ query/questao02.sql
@@ -1,4 +1,4 @@
-CREATE TABLE OR REPLACE `hackathon-a3-data.the_data_girls.q01_industria_ativa_opcao` (
+CREATE TABLE OR REPLACE `hackathon-a3-data.the_data_girls.q2_estabelecimento_fechado` (
     tipo	STRING,	
     opcao	STRING,
     municipio	STRING,	
@@ -6,8 +6,7 @@ CREATE TABLE OR REPLACE `hackathon-a3-data.the_data_girls.q01_industria_ativa_op
     quantidade	INTEGER,	
 )
 
-
-INSERT INTO `hackathon-a3-data.the_data_girls.q01_industria_ativa_opcao`
+INSERT INTO `hackathon-a3-data.the_data_girls.q2_estabelecimento_fechado`
 SELECT
     'SIMPLES' as tipo,
     opcao_pelo_simples as opcao,
@@ -15,9 +14,9 @@ SELECT
     FORMAT_DATE('%Y-%m',data_situ_cadastral) AS data_situacao_cadastro,
     COUNT(*) as quantidade
 FROM `hackathon-a3-data.the_data_girls.empresa_estabelecimento`
-WHERE situacao_cadastral = 'ATIVA'
-AND data_situ_cadastral > '2010-01-01'
+WHERE data_situ_cadastral > '2010-01-01'
 AND data_situ_cadastral  < '2021-06-01'
+AND situacao_cadastral IN ('NULA','BAIXADA')
 GROUP BY 1,2,3,4
 UNION ALL 
 SELECT 
@@ -27,7 +26,7 @@ SELECT
     FORMAT_DATE('%Y-%m',data_situ_cadastral) AS data_situacao_cadastro,
     COUNT(*) as quantidade 
 FROM `hackathon-a3-data.the_data_girls.empresa_estabelecimento`
-WHERE situacao_cadastral = 'ATIVA'
-AND data_situ_cadastral > '2010-01-01'
+WHERE data_situ_cadastral > '2010-01-01'
 AND data_situ_cadastral  < '2021-06-01'
+AND situacao_cadastral IN ('NULA','BAIXADA')
 GROUP BY 1,2,3,4
