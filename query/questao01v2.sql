2654e9e484c3740060720907d95493272be5b330
Carolina Dias <33383955+diascarolina@users.noreply.github.com>
2021-09-25 11:50:23 -0300

    update question 1 to add UF names

---
+++ query/questao01v2.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `hackathon-a3-data.the_data_girls.q01_v2_industria_ativa_opcao` (
+    tipo	STRING,	
+    opcao	STRING,
+    municipio	STRING,
+    uf STRING,
+    data_situacao_cadastro	STRING,	
+    quantidade	INTEGER,	
+);
+
+
+INSERT INTO `hackathon-a3-data.the_data_girls.q01_v2_industria_ativa_opcao`
+SELECT
+    'SIMPLES' as tipo,
+    opcao_pelo_simples as opcao,
+    municipio,
+    uf,
+    FORMAT_DATE('%Y-%m', data_situ_cadastral) AS data_situacao_cadastro,
+    COUNT(*) as quantidade
+FROM `hackathon-a3-data.the_data_girls.empresa_estabelecimento`
+WHERE situacao_cadastral = 'ATIVA'
+AND data_situ_cadastral >= '2010-01-01'
+AND data_situ_cadastral  <= '2021-06-30'
+GROUP BY 1, 2, 3, 4, 5
+UNION ALL 
+SELECT 
+    'MEI' as  tipo,
+    opcao_pelo_mei AS opcao,
+    municipio,
+    uf,
+    FORMAT_DATE('%Y-%m', data_situ_cadastral) AS data_situacao_cadastro,
+    COUNT(*) as quantidade 
+FROM `hackathon-a3-data.the_data_girls.empresa_estabelecimento`
+WHERE situacao_cadastral = 'ATIVA'
+AND data_situ_cadastral >= '2010-01-01'
+AND data_situ_cadastral  <= '2021-06-30'
+GROUP BY 1, 2, 3, 4, 5
